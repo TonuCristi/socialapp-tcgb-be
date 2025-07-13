870e0723207ecdff0dc0b2fc2f171cd9dbc776c5
Tonu Cristi <chrisdev2002@gmail.com>
2025-07-13 15:37:50 +0300

    feat: created the login functionality

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,8 +1,11 @@
-import register from "@/controllers/v1/auth/register";
 import { Router } from "express";
+import login from "@/controllers/v1/auth/login";
+import register from "@/controllers/v1/auth/register";
 
 const router = Router();
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 export default router;
