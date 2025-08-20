6652a2091c020a551adc05e3d005280fafe97930
Tonu Cristi <chrisdev2002@gmail.com>
2025-08-21 00:30:36 +0300

    feat: created add post comment fucntionality

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -2,13 +2,16 @@ import { model, Schema, Types } from "mongoose";
 
 export type Comment = {
   content: string;
+  parentId: Types.ObjectId;
   postId: Types.ObjectId;
   userId: Types.ObjectId;
+  createdAt: string;
 };
 
 const commentSchema = new Schema<Comment>(
   {
     content: String,
+    parentId: { type: Schema.Types.ObjectId, ref: "Comment" },
     postId: { type: Schema.Types.ObjectId, ref: "Post" },
     userId: { type: Schema.Types.ObjectId, ref: "User" },
   },
--- src/routes/v1/posts.route.ts
+++ src/routes/v1/posts.route.ts
@@ -4,6 +4,8 @@ import multer from "multer";
 import getUserPosts from "@/controllers/v1/posts/getUserPosts";
 import likePost from "@/controllers/v1/posts/likePost";
 import getPostLikes from "@/controllers/v1/posts/getPostLikes";
+import addPostComment from "@/controllers/v1/posts/addPostComment";
+import getPostComments from "@/controllers/v1/posts/getPostComments";
 
 const router = Router();
 
@@ -16,6 +18,10 @@ router.get("/get-user-posts", getUserPosts);
 
 router.post("/like-post/:postId", likePost);
 
+router.post("/add-post-comment/:postId", addPostComment);
+
 router.get("/get-post-likes/:postId", getPostLikes);
+
+router.get("/get-post-comments/:postId", getPostComments);
 
 export default router;
